3622e68dbf2aeeeab95f5fc7f82fa1a5c26c9582
Stefano Varesi <stefano.varesi@gmail.com>
2025-02-19 19:43:07 +0100

    refactor: WIP http production code moved

--- src/health_checker.rs
+++ src/health_checker.rs
@@ -3,12 +3,12 @@ use std::env;
 use std::str::FromStr;
 use std::time::Duration;
 use http::Method;
-use log::{debug, error, info};
-use reqwest::Client;
 
-use crate::health_checker::Reason::{StatusCode, Timeout};
+use crate::health_checker::Reason::{StatusCode};
 use redis::{Connection, ConnectionLike};
 use url::Url;
+
+use crate::health_checker::redis_health_checker::Http;
 
 #[cfg(test)]
 use mockall::automock;
@@ -176,60 +176,6 @@ fn load_configuration_from(
         timeout,
         status_code,
     })
-}
-
-struct Http;
-
-impl HealthChecker for Http {
-    // TODO receive only the required configuration
-    async fn check(&self, configuration: &Configuration) -> Result<State, NetworkError> {
-        let mut url = Url::parse("http://localhost").unwrap();
-        url.set_port(Some(configuration.port)).unwrap();
-        url.set_path(&configuration.path);
-
-        let client = Client::builder()
-            .timeout(configuration.timeout)
-            .build()
-            .unwrap();
-
-        let response = client
-            .request(configuration.method.clone(), url.as_ref())
-            .send()
-            .await;
-
-        debug!("received result from {}: {:?}", url, response);
-
-        let result: Result<State, NetworkError> = match response {
-            Ok(value) => {
-                if value.status() == configuration.status_code {
-                    Ok(State::Healthy)
-                } else {
-                    let reason = value
-                        .status()
-                        .canonical_reason()
-                        .unwrap_or("")
-                        .to_string();
-                    Ok(State::Unhealthy(StatusCode(value.status().as_u16(), reason)))
-                }
-            }
-            Err(e) => {
-                if e.is_timeout() {
-                    Ok(State::Unhealthy(Timeout(configuration.timeout)))
-                } else {
-                    Err(NetworkError {
-                        message: format!("network error: {}", e)
-                    })
-                }
-            }
-        };
-
-        match &result {
-            Ok(state) => info!("state {}", state),
-            Err(failure) => error!("{}", failure.message),
-        }
-
-        result
-    }
 }
 
 #[cfg_attr(test, automock)]
--- src/health_checker/redis_health_checker.rs
+++ src/health_checker/redis_health_checker.rs
@@ -1,3 +1,62 @@
+use crate::health_checker::Reason::{StatusCode, Timeout};
+use crate::health_checker::{Configuration, HealthChecker, NetworkError, State};
+use log::{debug, error, info};
+use reqwest::Client;
+use url::Url;
+
 #[cfg(test)]
 #[path = "./redis_health_checker_test.rs"]
 mod redis_test;
+
+pub struct Http;
+
+impl HealthChecker for Http {
+    // TODO receive only the required configuration
+    async fn check(&self, configuration: &Configuration) -> Result<State, NetworkError> {
+        let mut url = Url::parse("http://localhost").unwrap();
+        url.set_port(Some(configuration.port)).unwrap();
+        url.set_path(&configuration.path);
+
+        let client = Client::builder()
+            .timeout(configuration.timeout)
+            .build()
+            .unwrap();
+
+        let response = client
+            .request(configuration.method.clone(), url.as_ref())
+            .send()
+            .await;
+
+        debug!("received result from {}: {:?}", url, response);
+
+        let result: Result<State, NetworkError> = match response {
+            Ok(value) => {
+                if value.status() == configuration.status_code {
+                    Ok(State::Healthy)
+                } else {
+                    let reason = value.status().canonical_reason().unwrap_or("").to_string();
+                    Ok(State::Unhealthy(StatusCode(
+                        value.status().as_u16(),
+                        reason,
+                    )))
+                }
+            }
+            Err(e) => {
+                if e.is_timeout() {
+                    Ok(State::Unhealthy(Timeout(configuration.timeout)))
+                } else {
+                    Err(NetworkError {
+                        message: format!("network error: {}", e),
+                    })
+                }
+            }
+        };
+
+        match &result {
+            Ok(state) => info!("state {}", state),
+            Err(failure) => error!("{}", failure.message),
+        }
+
+        result
+    }
+}
